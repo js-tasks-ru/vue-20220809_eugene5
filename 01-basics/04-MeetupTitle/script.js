539e0e9d30817c15d5f80ec97e11ef34f4b5310f
Eugene Parshukov <ev.klit@gmail.com>
2022-08-30 12:29:01 +0300

    Добавлена задача MeetupTitle

--- 01-basics/04-MeetupTitle/script.js
+++ 01-basics/04-MeetupTitle/script.js
@@ -1,17 +1,37 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js'
 
-const API_URL = 'https://course-vue.javascript.ru/api';
+const API_URL = 'https://course-vue.javascript.ru/api'
 
 function fetchMeetupById(meetupId) {
   return fetch(`${API_URL}/meetups/${meetupId}`).then((response) => {
     if (response.ok) {
-      return response.json();
+      return response.json()
     } else {
       return response.json().then((error) => {
-        throw error;
-      });
+        throw error
+      })
     }
-  });
+  })
 }
 
 // Требуется создать Vue приложение
+createApp({
+  data() {
+    return {
+      meetup: null,
+      meetupId: 1,
+    }
+  },
+  created() {
+    fetchMeetupById(this.meetupId).then((meetup) => {
+      this.meetup = meetup
+    })
+  },
+  watch: {
+    meetupId(id) {
+      fetchMeetupById(id).then((meetup) => {
+        this.meetup = meetup
+      })
+    },
+  },
+}).mount('#app')
